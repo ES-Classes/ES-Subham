2eff05decc9aab93d8785afb38990ff1199e950e
21subham <subhamrajbhandari@gmail.com>
2022-04-03 11:40:26 +0545

    update

---
+++ CLasses/2-datatypes.js
@@ -0,0 +1,32 @@
+console.log("Hello World");
+
+var  age = 45;
+age = 'char';
+
+//Datatypes - 8 
+//Numbers - FP
+var population = 21345;
+console.log(1/0);
+console.log("kek"/32);
+var bigNmber = 2343453534643643365433333;
+
+//String - Sequence of characters
+var name = 'Subham Rajbhandari';
+name = 'Subham Rajbhandari';
+name = 'Subham Rajbhandari';
+
+console.log(`My name is ${name}`);
+
+//Boolean - true or false
+var isAnimal = true;
+
+//Undefined
+var year;
+
+console.log(year);
+
+//Null
+var u = null;
+
+//Object
+//Symbol
