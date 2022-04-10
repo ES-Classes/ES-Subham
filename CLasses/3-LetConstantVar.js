7c6129545519c9e1c6a1299ba861fbac7dff99f4
21subham <subhamrajbhandari@gmail.com>
2022-04-10 11:04:55 +0545

    new

--- CLasses/3-LetConstantVar.js
+++ CLasses/3-LetConstantVar.js
@@ -3,4 +3,22 @@ let b = 34;
 const c = 67;
 a = 567;
 
-console.log(a);
+console.log(c);
+
+let z = x;
+
+let f = 34
+
+if ( f % 2 == 0)
+console.log("even")
+else
+console.log("odd")
+
+isEven  = x & 1 == 0;
+
+let p = x << 1; 
+let q = y >> 1;
+let r = ~isEven;
+
+//Comparision
+let s = x =='90';
