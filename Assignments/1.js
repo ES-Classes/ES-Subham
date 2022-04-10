012c60d0a9a96cbdfe25445e766b07c3e82599c8
21subham <subhamrajbhandari@gmail.com>
2022-04-10 11:44:56 +0545

    new

---
+++ Assignments/1.js
@@ -0,0 +1,21 @@
+
+let massP = 78;
+let heightP = 1.78;
+
+let massR = 92;
+let heightR = 1.92;
+
+let BMIP = massP / heightP^2;
+
+console.log(BMIP);
+
+let BMIR = massR / heightR^2;
+
+console.log(BMIR);
+
+
+
+
+
+
+
