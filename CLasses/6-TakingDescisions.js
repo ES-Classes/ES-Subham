40a3a9678c818572fb7a6b7adf96f71047ae9cf5
21subham <subhamrajbhandari@gmail.com>
2022-04-17 11:07:05 +0545

    move

---
+++ CLasses/6-TakingDescisions.js
@@ -0,0 +1,43 @@
+let x = 50;
+
+if ( x < 50)
+{
+    let y = 89;
+    console.log();
+}
+else if(x < 70)
+{
+    //some code
+}
+else
+{
+
+
+}
+
+//Chaining and nesting
+
+//console.log(y);
+
+//switch
+let currentTime = new Date("2020/4/5");
+//console.log(currentTIme);
+let day = currentTime.getDay();
+
+switch(day)
+{
+    case 0:console.log("It's sunday");
+    break;
+    case 1:console.log("It's monnday");
+    break;
+    case 2:console.log("It's tuesday");
+    break;
+    case 3:console.log("It's wednesday");
+    break;
+    case 4:console.log("It's thursday");
+    break;
+    case 5:console.log("It's friday");
+    break;
+    default:console.log("It's saturday");
+}
+
