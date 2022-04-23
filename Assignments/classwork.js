30b07a01dc8a8c7cf6913e39a7e7805feabe33de
21subham <subhamrajbhandari@gmail.com>
2022-04-23 10:20:35 +0545

    3

--- Assignments/classwork.js
+++ Assignments/classwork.js
@@ -1,3 +1,12 @@
+/* ## Arjun wants to build a very simple tip calculator for whenever he goes eating in a restaurant. In his country, it's usual to tip 10% if the bill value is between 50 and 300. If the value is different, the tip is 15%.
+
+## Your tasks:
+1. Calculate the tip, depending on the bill value.
+2. Print a string to the console containing the bill value, the tip, and the final value
+(bill + tip). Example: 
+    >“The bill was Rs. 200, the tip was Rs. 20, and the total is Rs. 220”
+## Test data:
+* Test for bill values 275, 40 and 430 */
 
 function tip(n)
 {
@@ -11,7 +20,5 @@ function tip(n)
 
 let x = 250;
 console.log(tip(x));
-console.log(x);
-
 
 console.log('The bill was:',x,', The tip was:',tip(x),',The total was:',x+tip(x));
